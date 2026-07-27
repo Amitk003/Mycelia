cd9279df04b3b1168110d44c630a1598756bba99
amitee <amitkumar023410@gmail.com>
2026-07-27 21:40:26 +0530

    fix signaling server message relay logic

--- signaling-server/src/index.js
+++ signaling-server/src/index.js
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const PORT = process.env.PORT || 8080;
 
@@ -8,8 +8,12 @@ server.on("connection", (socket) => {
   console.log("peer connected");
 
   socket.on("message", (data) => {
-    const message = JSON.parse(data.toString());
-    console.log("received:", message.type);
+    // Relay signaling messages to all other connected peers
+    server.clients.forEach((client) => {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
   });
 
   socket.on("close", () => {
